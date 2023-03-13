c85c5b5956ff50b5931a5364265a4119e9d18d83
Daniel Filho <daniel.filho@123loadboard.com>
2023-03-13 09:44:24 -0400

    formatting config

--- services/logger.js
+++ services/logger.js
@@ -1,4 +1,6 @@
 export const Log = cleanFormat => (str) => {
-    str = str.replace(/\x1b\[\d{1,2}m/g, '')
+    if(cleanFormat){
+        str = str.replace(/\x1b\[\d{1,2}m/g, '')
+    }
     console.log(str);
 }
